30e6e54082a2720f3942b046fedba8e6666b01c4
huchao <huchao@huchaodeMacBook-Pro.local>
2015-08-14 18:52:21 +0800

    初始化项目

---
+++ sinaweibo/sinaweibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sinaweibo
+//
+//  Created by huchao on 15/8/14.
+//  Copyright (c) 2015年 huchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sinaweibo/sinaweibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sinaweibo
+//
+//  Created by huchao on 15/8/14.
+//  Copyright (c) 2015年 huchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
